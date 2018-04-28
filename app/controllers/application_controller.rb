1dcc259ca01f2403d336b4258b2b5e882ea23f7e
Hoon Lee <yl.hoony@gmail.com>
2018-04-28 00:27:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user
 
   def set_username
-    resp = Faraday.get("https://api.github.com/user") do |req|
-      req.params["access_token"] = session[:token]
-    end
+    # resp = Faraday.get("https://api.github.com/user") do |req|
+    #   req.params["access_token"] = session[:token]
+    # end
+
+    resp = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
 
     body = JSON.parse(resp.body)
     session[:username] = body["login"]
@@ -19,27 +21,18 @@ class ApplicationController < ActionController::Base
 
   private
 
-    # def authenticate_user
-    #   # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
-    #   # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
-    # 
-    #   client_id = ENV['GITHUB_CLIENT_ID']
-    #   # redirect_uri = CGI.escape("http://localhost:3000/auth")
-    #   github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&scope=public_repo"
-    #   set_username unless current_username
-    #   redirect_to github_url unless logged_in?
-    # end
-    # 
-    # def logged_in?
-    #   !!session[:token]
-    # end
-
     def authenticate_user
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+      # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
+      # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
+
+      client_id = ENV['GITHUB_CLIENT_ID']
+      redirect_uri = CGI.escape("http://localhost:3000/auth")
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}&redirect_uri=#{redirect_uri}&scope=repo"
+      set_username unless current_username
+      redirect_to github_url unless logged_in?
     end
 
     def logged_in?
       !!session[:token]
     end
-    
 end
